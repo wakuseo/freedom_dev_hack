9852c25eb34b7967fb9f7c252536fe51fff6a9e7
Denis Genitoni <denisg96@falco96.com>
2018-10-20 21:11:02 +0200

    aggiunto pulsante registrazione navbar e aggiunto componente movimenti economici (per la stampa delle ultime transazioni)

--- iGruppiApp/src/app/app.module.ts
+++ iGruppiApp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MovementComponent } from './movement/movement.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MovementComponent
   ],
   imports: [
     BrowserModule
