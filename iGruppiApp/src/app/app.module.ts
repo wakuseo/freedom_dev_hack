a22a295c40d33a1cc6e123e99a08948a1347be0c
wakuseo <philipwaku@gmail.com>
2018-10-21 00:17:59 +0200

    creato login/dashboard component

--- iGruppiApp/src/app/app.module.ts
+++ iGruppiApp/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
