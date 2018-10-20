f5c6ece0f99e10914ac2c7c711a245d6c7b3aeb2
raffa96 <pocho962010@hotmail.it>
2018-10-20 18:46:41 +0200

    Dipendenza popper.js

--- iGruppiApp/src/app/navbar/navbar.component.ts
+++ iGruppiApp/src/app/navbar/navbar.component.ts
@@ -6,15 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  navbarOpen: boolean = false;
-
   constructor() { }
 
   ngOnInit() {
   }
-
-  toggleNavbar() {
-    this.navbarOpen = !this.navbarOpen;
-  }
-
 }
